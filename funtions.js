0018d8c7d9678f819f2b47092ddfb50816879048
armeenabbas <armeen.abbasali@gmail.com>
2024-05-12 09:34:47 +0500

    My Function

---
+++ funtions.js
@@ -0,0 +1,11 @@
+"use strict";
+function greet() {
+    console.log("hello ruf");
+}
+function sum() {
+    console.log(2 + 2);
+}
+greet();
+sum();
+sum();
+greet();
---
+++ funtions.ts
@@ -0,0 +1,13 @@
+function greet () {
+
+console.log("hello ruf")
+}
+function sum(){
+    console.log(2+2)
+
+}
+
+greet();
+sum();
+sum();
+greet();
